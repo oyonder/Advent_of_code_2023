9984a47cf37004ab2ad4547954a3a3149610c26f
Özlem Yönder <ozlemyonder@Ozlems-MBP.fritz.box>
2023-12-01 15:58:42 +0100

    The solution to part 1 is added.

--- 01.py
+++ 01.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 import regex as re
 
-f = open("input01.txt", "r")
+part1 = input("Is this for part 1? Y/N: ") in ['Y','y']
 
+try:
+   f = open("input01.txt", "r")
+except:
+   f = open(input("Enter the input file name, e.g. input01.txt: "), "r")
+
+"""
+A function to find the first numerical value of a string
+"""
 def get_the_first(text):
     the_first = 0
     found = False
@@ -19,23 +27,26 @@ def get_the_first(text):
 answer = 0
 numbers = {"one":"1","two":"2","three":"3","four":"4","five":"5","six":"6","seven":"7","eight":"8","nine":"9"}
 
-for line in f.read().strip().split('\n'):    
-    print('before', line)
-    matches = []
-    spans   = []
-    result = re.findall("one|two|three|four|five|six|seven|eight|nine", line, overlapped = True)
+for line in f.read().strip().split('\n'):
 
-    if len(result) == 1:
-       line = line.split(result[0])[0] + numbers[result[0]] + line.split(result[0])[-1]
-    elif len(result) >= 2:
-       line1 = line.split(result[0])[0] + numbers[result[0]] + line.split(result[0])[-1]
-       line2 = line.split(result[-1])[0] + numbers[result[-1]] + line.split(result[-1])[-1]
-       line = line1 + line2
-    print('after', line)
+    if not part1:
+        # Find all the matches which are written as text
+        result = re.findall("one|two|three|four|five|six|seven|eight|nine", line, overlapped = True)
 
+        # If there are any, replace them with numbers
+        if len(result) == 1:
+           line = line.split(result[0])[0] + numbers[result[0]] + line.split(result[0])[-1]
+        elif len(result) >= 2:
+           line1 = line.split(result[0])[0] + numbers[result[0]] + line.split(result[0])[-1]
+           line2 = line.split(result[-1])[0] + numbers[result[-1]] + line.split(result[-1])[-1]
+           line = line1 + line2
+
+    # Now find the first and last digit in the modified text line
     first_digit  = get_the_first(line)
     second_digit = get_the_first(line[::-1])
+
+    # Sum them up!
     answer += int(str(first_digit)+str(second_digit))
 
-print(answer)
+print("The answer is: ", answer)
 
