186f1593cde7e9b4a104d9c09f22d977283e77c9
Özlem Yönder <ozlemyonder@Ozlems-MBP.fritz.box>
2023-12-01 15:57:44 +0100

    The solution to day 1 is added.

---
+++ 01.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+import regex as re
+
+f = open("input01.txt", "r")
+
+def get_the_first(text):
+    the_first = 0
+    found = False
+    while not found:
+       for char in text.strip():
+           try:
+               found = True
+               the_first = int(char) 
+               break
+           except:
+               continue
+    return the_first
+
+answer = 0
+numbers = {"one":"1","two":"2","three":"3","four":"4","five":"5","six":"6","seven":"7","eight":"8","nine":"9"}
+
+for line in f.read().strip().split('\n'):    
+    print('before', line)
+    matches = []
+    spans   = []
+    result = re.findall("one|two|three|four|five|six|seven|eight|nine", line, overlapped = True)
+
+    if len(result) == 1:
+       line = line.split(result[0])[0] + numbers[result[0]] + line.split(result[0])[-1]
+    elif len(result) >= 2:
+       line1 = line.split(result[0])[0] + numbers[result[0]] + line.split(result[0])[-1]
+       line2 = line.split(result[-1])[0] + numbers[result[-1]] + line.split(result[-1])[-1]
+       line = line1 + line2
+    print('after', line)
+
+    first_digit  = get_the_first(line)
+    second_digit = get_the_first(line[::-1])
+    answer += int(str(first_digit)+str(second_digit))
+
+print(answer)
+
