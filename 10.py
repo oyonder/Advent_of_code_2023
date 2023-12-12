48477a4a00641692323387182315ae19675cc922
Özlem Yönder <ozlemyonder@Ozlems-MacBook-Pro.local>
2023-12-12 11:48:50 +0100

    Explanations to Day 10 is added

--- 10.py
+++ 10.py
@@ -17,7 +17,49 @@ def find_start(lines):
                  return (y,x)
     return None
 
+
+def find_two_possibilities(start_loc, lines):
+    """
+    Finds the two possibilites near the starting location.
+    One will be the starting point, the other one will be the end point.
+
+    Args:
+    start_loc(tuple): starting location (where 'S' is)
+    lines(list): the lines in the files
+
+    Returns:
+    two_possibilities(list): returns list of tuples with the two possible 
+                             directions to go from the starting locaion. 
+    """
+    y0,x0 = start_loc[0], start_loc[1]
+    two_possibilities = []
+    # Up
+    if lines[y0-1][x0] in ['F','|','7']:
+        two_possibilities.append((y0-1,x0))
+    # Down
+    if lines[y0+1][x0] in ['L','|','J']:
+        two_possibilities.append((y0+1,x0))
+    # Left
+    if lines[y0][x0-1] in ['-','F','L']:
+        two_possibilities.append((y0,x0-1))
+    # Right
+    if lines[y0][x0+1] in ['L','-','J']:
+        two_possibilities.append((y0,x0+1))
+    return two_possibilities[0], two_possibilities[1]
+
+
 def find_next(current, visited, lines):
+    """
+    Finds the next element given the current location and its symbol.
+    
+    Args:
+    current(tuple): Location of the current pipe
+    visited(list): List of tuples with the location of the visited pipes which make up the loop
+    lines(list): The lines of the input file
+
+    Returns:
+    (y,x): The location of the next pipe as a tuple
+    """
     y,x = current[0], current[1]
     current_pipe = lines[y][x]
 
@@ -70,6 +112,18 @@ def find_next(current, visited, lines):
             return (y+1,x)
 
 def count_enclosed_area(lines,visited):
+    """
+    Finds whether a point is inside or outside of the area enclosed using
+    ray casting algorithm (also known as crossing number algorithm or even-odd rule algorithm).
+    Then, counts the number of points inside the enclosed area.
+
+    Args:
+    lines(list): The lines in the input file
+    visited(list): A list of tuples, which are the locations of the points which make up the loop
+
+    Returns:
+    counter(int): The total number of points inside the enclosed area
+    """
     counter = 0
     last_pipe = '|'
     for y,line in enumerate(lines):
@@ -91,39 +145,33 @@ def count_enclosed_area(lines,visited):
                      counter += 1
     return counter
 
-
+ 
 
 def main():
-
-    verbose = False
-
-    part1 = input("Is this for part 1? Y/N: ") in ['Y','y']
 
     file_name = input("Enter the input file name, e.g. input.txt: ")
     lines = read_file(file_name)
 
+    # Find the location of the starting pipe 
     start_loc = find_start(lines)
-    y0,x0 = start_loc[0], start_loc[1]
-    #current = (y0, x0+1) # Right - 
-    #end_loc = (y0+1, x0) # Down  | 
-    current = (y0-1,x0) # Up    | 
-    end_loc = (y0,x0+1) # Right J
+    # Find the two possiblities of movement from the start
+    current, end_loc = find_two_possibilities(start_loc, lines)
+
+    # Make a list of visited locations which make up the loop and count the number of steps
     visited = [start_loc, current]
-
     counter = 2
-
     while current != end_loc:
         current = find_next(current, visited, lines)
         y,x = current[0],current[1]
-        #print('current', current, lines[y][x])
         counter += 1
         visited.append(current)
 
+    # Part 1
     print('Total number of steps is', counter)
     print('The farthest point is %d steps away!' %(counter/2))
 
-    answer = count_enclosed_area(lines, visited)
-    print(answer)
+    # Part 2
+    print('The number of tiles enclosed by the loop is', count_enclosed_area(lines, visited))
 
 if __name__ == "__main__":
     main()
